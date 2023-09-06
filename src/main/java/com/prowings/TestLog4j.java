f0ff33a0c8857ff819bc7bb961229a8fc72439f5
JD <jayprowings@gmail.com>
2023-09-06 12:12:20 +0530

    6 Sept - appenders

--- src/main/java/com/prowings/TestLog4j.java
+++ src/main/java/com/prowings/TestLog4j.java
@@ -11,15 +11,27 @@ public class TestLog4j {
 	public static final Logger logger = LoggerFactory.getLogger(TestLog4j.class);
 //	    public static final Logger logger = LogManager.getLogger(TestLog4j.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		long startTime = System.currentTimeMillis();
+        long duration = 10 * 1000; // 30 seconds in milliseconds
+
+        while (System.currentTimeMillis() - startTime < duration) {
+        	
+    		logger.info("An INFO message");
+            logger.debug("A DEBUB message");
+            logger.trace("A TRACE message");
+            logger.warn("A WARN message");
+            logger.error("A ERROR message");
+//            logger.fatal("A FATAL message");
+
+        }
 		
-		logger.info("An INFO message");
-        logger.debug("A DEBUB message");
-        logger.trace("A TRACE message");
-        logger.warn("A WARN message");
-        logger.error("A ERROR message");
-//        logger.fatal("A FATAL message");
-		
+        
+//        Thread.sleep(15000);
+//        TestLog4j2.myMain();
+        
+        System.out.println("Run Complete!!!");
 	}
 
 }
